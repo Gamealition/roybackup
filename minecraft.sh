dea9cca0191412c1af5f23974b0e69b3b26732f0
Roy Curtis <roy.adrian.curtis+github@gmail.com>
2015-01-16 20:47:27 +0000

    - added more crontab examples
    - moved over minecraft backup script and exclusions

---
+++ minecraft.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Minecraft directory backup script by RoyCurtis
+
+# CONFIGURATION:
+
+# Where backups are to be kept
+BACKUPDIR=/home/backups/minecraft
+EXCLUDE=minecraft.exclude
+
+# CONSTANTS:
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games
+cd $DIR
+
+# SCRIPT:
+echo -e "[MINECRAFT] Calculating backup size..."
+
+SIZE=`du -sm --exclude-from=$EXCLUDE /home/$1 2> /dev/null | cut -f 1`
+TIMESTAMP=`date +%H%M-%d-%m-%Y`
+BACKUPPATH=${BACKUPDIR}/$1-${TIMESTAMP}.tar.bz
+
+echo -e "[MINECRAFT] Performing ${SIZE}MB /home/minecraft/$1 backup to ${BACKUPPATH}..."
+tar --exclude-from=$EXCLUDE \
+    -cf - /home/minecraft/$1 | pv -p -e -r -b -s ${SIZE}m | bzip2 > $BACKUPPATH
+echo -e "[MINECRAFT] Finished backup to ${BACKUPPATH}"
